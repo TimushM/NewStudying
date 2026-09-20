d043d80614c937c81a34124c713bdb4c5d6b490f
TimushM <uman.04062017@gmail.com>
2026-09-20 14:49:56 +0200

    2

--- src/main/java/sk/upjs/sk/Main.java
+++ src/main/java/sk/upjs/sk/Main.java
@@ -3,6 +3,7 @@ package sk.upjs.sk;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Arsenii");
+        System.out.println("Hello World!");
     }
 
 }
